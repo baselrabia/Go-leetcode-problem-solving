6620c266b41da4c0d9a0fcac4a9624568ba3373b
Basel Rabia <baselrabia@gmail.com>
2022-06-27 17:51:57 +0200

    Create main.go

---
+++ 1. Two Sum/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+// Approach 1: Brute Force
+
+func twoSumForce(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return nil
+}
+
+// Approach 3: One-pass Hash Table
+
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+
+		comp := target - nums[i]
+
+		_, exist := m[comp]
+
+		if exist {
+			return []int{m[comp], i}
+		}
+
+		m[nums[i]] = i
+	}
+
+	fmt.Println("map:", m)
+	return nil
+
+	 
+}
+
+func main() {
+	r := twoSum([]int{2, 7, 11, 15}, 9)
+	fmt.Println(r)
+}
