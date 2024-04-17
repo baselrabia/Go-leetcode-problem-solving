1e411b57456bdfc642089d004603a423a72fbbed
Basel Rabia <baselrabia@gmail.com>
2024-04-17 14:42:11 +0300

    Solve 988-988-Smallest-String-Starting-From-Leaf in Go with Time Beats: 87.50% and Memory Beats: 62.50%

---
+++ 988-Smallest-String-Starting-From-Leaf/988-Smallest-String-Starting-From-Leaf.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func smallestFromLeaf(root *TreeNode) string {
+    var helper func(*TreeNode, string) string
+    
+    helper = func(node *TreeNode, current string) string {
+        if node == nil {
+            return ""
+        }
+        
+        ch := byte('a' + node.Val)
+        current = string(ch) + current
+
+        if node.Left == nil && node.Right == nil {
+            return current
+        }
+
+        leftStr := helper(node.Left, current)
+        rightStr := helper(node.Right, current)
+
+        if leftStr == "" {
+            return rightStr
+        }
+        if rightStr == "" {
+            return leftStr
+        }
+        if leftStr < rightStr {
+            return leftStr
+        }
+        return rightStr
+    }
+    
+    return helper(root, "")
+}
+
+ 
